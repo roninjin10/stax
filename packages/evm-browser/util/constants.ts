44035fe6619c84993fcc83a408fcb74076255116
Will Cory <willcory@Wills-MacBook-Pro.local>
2022-12-26 23:32:29 -0800

    renameeverything

--- packages/evm-browser/util/constants.ts
+++
@@ -1,5 +0,0 @@
-export const GITHUB_REPO_URL = 'https://github.com/comitylabs/evm.codes'
-
-// Currently active hardfork from the ones available:
-// See: https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/common/src/hardforks
-export const CURRENT_FORK = 'merge'
