7c4f8ca829640bd1aa8d9651dcd55f75f744047f
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-01-13 21:08:58 -0800

    :zap: make a stupid counter contract

--- packages/contracts/script/Deploy.t.sol
+++ packages/contracts/script/Deploy.t.sol
@@ -3,14 +3,17 @@ pragma solidity ^0.8.17;
 
 import {Script} from "forge-std/Script.sol";
 import {AppEntrypoint} from "../src/AppEntrypoint.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract Deploy is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
 
+
         vm.startBroadcast(deployerPrivateKey);
 
         AppEntrypoint appEntrypoint = new AppEntrypoint();
+        Counter counter = new Counter();
 
         vm.stopBroadcast();
     }
