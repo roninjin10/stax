3cab2e454beb8fa51dfe3e59fdc4d8caf0f6f10d
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-01-29 23:25:38 -0800

    :package: add next app

---
+++ packages/next-app/src/wagmi.ts
@@ -0,0 +1,11 @@
+import { getDefaultClient } from 'connectkit'
+import { createClient } from 'wagmi'
+
+export const client = createClient(
+  getDefaultClient({
+    autoConnect: true,
+    appName: 'My wagmi + ConnectKit App',
+    alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!,
+    infuraId: process.env.NEXT_PUBLIC_INFURA_API_KEY!,
+  })
+)
