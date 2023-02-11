0e69d5f632edd739d02638222b201a3b8699e271
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-11 14:18:54 -0800

    :construction: write foundry project to temp directory

--- packages/babel-plugin-sol/src/index.ts
+++ packages/babel-plugin-sol/src/index.ts
@@ -1,17 +1,63 @@
 import { /*NodePath, template,*/ types as t } from '@babel/core'
 import { declare } from '@babel/helper-plugin-utils'
+import * as fs from 'fs/promises'
+import * as os from 'os'
+import * as path from 'path'
+import { z } from 'zod'
 
-export interface Options {
-  loose?: boolean
+const isSolidityVersion = (version: string) => {
+  const regex = /^\d+\.\d+\.\d+$/
+  return regex.test(version)
 }
 
+export const optionsValidator = z.object({
+  // solc must be a version e.g. 0.8.17
+  solc: z
+    .string()
+    .refine(isSolidityVersion)
+    .optional()
+    .default('0.8.17')
+    .describe('solc version'),
+})
+
+export type Options = z.infer<typeof optionsValidator>
+
 export default declare((api, options: Options) => {
+  let solc: string
+  try {
+    solc = optionsValidator.parse(options).solc
+  } catch (e) {
+    console.error(e)
+    throw new Error('ts-sol babel plugin options are invalid')
+  }
+
   api.assertVersion(7)
-  console.log(options)
+
+  const tempDir = os.tmpdir()
+
+  const tempForgeProjectPath = path.join(tempDir, '__ts-sol-forge-project__')
+
+  const foundryTomlPath = path.join(tempForgeProjectPath, 'foundry.toml')
+
+  const foundryToml = `
+[project]
+name = "temp-ts-sol-forge-project"
+src = "src"
+out = "dist"
+solc = ${solc}
+`
+
+  const createProjectPromise = fs.mkdir(tempForgeProjectPath).then(() => {
+    return Promise.all([
+      fs.writeFile(foundryTomlPath, foundryToml, { encoding: 'utf-8' }),
+      fs.mkdir(path.join(tempForgeProjectPath, 'src')),
+    ])
+  })
+
   return {
     name: 'ts-sol',
     visitor: {
-      TaggedTemplateExpression(path) {
+      TaggedTemplateExpression: async (path) => {
         const {
           node: { tag, quasi },
         } = path
@@ -21,6 +67,8 @@ export default declare((api, options: Options) => {
         if (!isTsSolTag) {
           return
         }
+
+        await createProjectPromise
 
         /*
         we aren't handling string interpelation yet
