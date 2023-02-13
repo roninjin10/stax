32c263ffe18d287b8e6ce8f2110820d6817d3f89
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-13 12:58:12 -0800

    :truck: add vite-app back so previous links don't break for a couple months

---
+++ apps/vite-app/babel.config.js
@@ -0,0 +1,5 @@
+export default (api) => {
+  return {
+    plugins: ['@roninjin10/babel-plugin-sol'],
+  }
+}
---
+++ apps/vite-app/src/App.tsx
@@ -0,0 +1,37 @@
+import { run, tsSol } from '@roninjin10/ts-sol'
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+
+const forgeScript = tsSol`
+contract Script {
+    function run() external returns (string memory) {
+        return "Hello, World!";
+    }
+}
+`
+
+const w = window as any
+w.process = {
+  env: {
+    DEBUG: 'ethjs',
+  },
+}
+
+export const App = () => {
+  const [isEnabled, setIsEnabled] = useState(false)
+  const { data, error, isLoading } = useQuery(
+    ['helloWorldQuery'],
+    async () => {
+      return run(forgeScript)
+    },
+    { enabled: isEnabled },
+  )
+  return (
+    <div>
+      <button onClick={() => setIsEnabled(!isEnabled)}>Click to run tx</button>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {(error as Error).message}</div>}
+      {data && <div>{data}</div>}
+    </div>
+  )
+}
